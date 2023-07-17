17c5f26146b2d57f8e5baf9a15ad5bdcdfeafb3f
natnael-emaye <natnaelabebegualu@gmail.com>
2023-07-17 12:34:25 +0300

    Vite + React + Tailwind starter

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div className="bg-slate-950 w-full h-screen flex items-center justify-center " >
         <h1 className="text-3xl font-bold text-white ">
-          Vite + React + Tailwind Startup
+          Vite + React + Tailwind starter
         </h1>
       </div>
     </>
