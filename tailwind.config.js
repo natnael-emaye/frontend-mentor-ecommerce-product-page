5ff3bc243b6ac209f485c117da6cf92c0bc94157
natnael-emaye <natnaelabebegualu@gmail.com>
2023-07-25 15:48:07 +0300

    add custom colors for the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange": 'hsl(26, 100%, 55%)',
+        'Paleorange': 'hsl(25, 100%, 94%)',
+        "verydarkblue": "hsl(220, 13%, 13%)",
+        "darkgrayishblue": "hsl(219, 9%, 45%)",
+        "grayishblue: hsl": "(220, 14%, 75%)",
+        "lightgrayishblue": "hsl(223, 64%, 98%)",
+        "white": "hsl(0, 0%, 100%)",
+        "black": "hsl(0, 0%, 0%)",
+      },
+    },
   },
   plugins: [],
 };
