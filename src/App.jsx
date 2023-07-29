d03f6d2b501dcd1439caf7d2d7b091e67b221b37
natnael-emaye <natnaelabebegualu@gmail.com>
2023-07-29 22:44:46 +0300

    change width

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="max-w-[1440px] mx-auto px-14 ">
-      <Header />
-    </div>
+    <>
+      <div className="max-w-[1440px] mx-auto w-[80vw] h-screen flex flex-col  ">
+        <Header />
+        <Main />
+      </div>
+    </>
   );
 }
 
