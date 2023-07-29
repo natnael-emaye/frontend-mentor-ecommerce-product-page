eb8ebb123de962cd601c2a17b659a49000a2216b
natnael-emaye <natnaelabebegualu@gmail.com>
2023-07-29 22:45:29 +0300

    completed

---
+++ src/components/LargeProductImage.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+export default function LargeProductImage({ image }) {
+  return <img className="w-full h-[360px] rounded-xl " src={image} alt="large product Image" />;
+}
